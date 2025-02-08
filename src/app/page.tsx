818f3b4a67ddf0b5fcce51d9b4fe9659b24d63a1
santosherrera-lopez <santoslopez890@gmail.com>
2025-02-07 20:32:45 -0500

    added map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import dynamic from "next/dynamic";
+import FoodPantryMap from "./map"; 
 
 export default function Home() {
   return (
@@ -12,6 +14,10 @@ export default function Home() {
           height={38}
           priority
         />
+
+{/* Food Pantry Map Component */}
+<FoodPantryMap />
+
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
